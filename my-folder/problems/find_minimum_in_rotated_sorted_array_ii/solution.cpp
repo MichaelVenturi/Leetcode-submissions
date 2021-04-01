97f4169ff0ff47a4a80ecd834ff78f4441cccd5a
Michael Venturi <michaelventuri58@aol.com>
2021-04-01 03:28:48 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array II (cpp)

---
+++ my-folder/problems/find_minimum_in_rotated_sorted_array_ii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        int min = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(min > nums[i]){
+                min = nums[i];
+                break;
+            }
+        }
+        
+        return min;
+        
+    }
+};
