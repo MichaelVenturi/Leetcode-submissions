5b4d1b0a2e81761f617d0df32cf3d8d62b90f22c
Michael Venturi <michaelventuri58@aol.com>
2023-02-03 18:10:54 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- my-folder/problems/reverse_integer/solution.cpp
+++ my-folder/problems/reverse_integer/solution.cpp
@@ -3,22 +3,20 @@ public:
     int reverse(int x) {
         if(x > INT_MAX || x < INT_MIN) return 0;
         int r = 0;
-        int digits = 0;
-        int y = x;
+       // int digits = 0;
+       // int y = x;
         int z = 0;
 
-        while(abs(y) > 0){
-            y /= 10;
-            digits++;
-        }
-        cout << digits << endl;
+       
         
         while(abs(x) > 0){
             r = x % 10;
             x /= 10;
             cout << r << endl;
-            if(z+r*pow(10, digits-1) > INT_MAX || z+r*pow(10, digits-1) < INT_MIN) return 0;
-            z += r * pow(10, --digits);
+            if(z > INT_MAX/10 || 
+                z < INT_MIN/10)            
+                return 0;
+            z = z*10+r;
 
             //cout << r << " "  << x << " " << digits << " " << z;
             
