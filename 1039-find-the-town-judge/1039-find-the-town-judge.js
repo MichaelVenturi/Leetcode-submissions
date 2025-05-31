416927ba3f24326fe4d173bd761532f7dbe0c59c
Michael Venturi <michaelventuri58@aol.com>
2025-05-31 03:15:53 -0400

    Time: 5 ms (74.41%), Space: 63.4 MB (54.5%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.js
+++ 1039-find-the-town-judge/1039-find-the-town-judge.js
@@ -5,26 +5,13 @@
  */
 var findJudge = function(n, trust) {
     if(n === 1) return 1;
-    const adj = createList(trust);
-    for(const [key, val] of Object.entries(adj)){
-        if(val[0] === (n-1) && val[1] === 0) return +key;
+    const list = Array.from({length: n+1}, () => [0, 0]);
+    for(const [a, b] of trust){
+        list[a][0]++;
+        list[b][1]++;
+    }
+    for(let i = 1; i <= n; i++){
+        if(list[i][1] === n - 1 && list[i][0] === 0) return i
     }
     return -1;
 };
-
-const createList = (trust) => {
-    const adjList = {};
-    for(const [a, b] of trust){
-        if(!adjList[a]) adjList[a] = [0, 0];
-        if(!adjList[b]) adjList[b] = [0, 0];
-        adjList[a][1]++;
-        adjList[b][0]++;
-    }
-    return adjList;
-}
-
-/*
-    {
-       person: [how many trust them, how many they trust]
-    }
-*/
