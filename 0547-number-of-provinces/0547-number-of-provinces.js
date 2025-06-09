30362be6a637957919f34e8688c6ffebc36f1b15
Michael Venturi <michaelventuri58@aol.com>
2025-06-09 17:37:13 -0400

    Time: 1 ms (93.46%), Space: 58.1 MB (40.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -4,19 +4,10 @@
  */
 var findCircleNum = function(isConnected) {
     const n = isConnected.length;
-    const adj = Array.from({length: n}, () => []);
-    for(let i = 0; i < n - 1; i++){
-        for(let j = i + 1; j < n; j++){
-            if(isConnected[i][j] === 1){
-                adj[i].push(j);
-                adj[j].push(i);
-            }
-        }
-    }
+
     const visited = new Array(n).fill(0);
     let count = 0;
     for(let i = 0; i < n; i++){
-        console.log(visited);
         if(visited[i] !== 1){
             count++;
             visited[i] = 1;
@@ -24,10 +15,11 @@ var findCircleNum = function(isConnected) {
             q.push(i);
             while(q.length > 0){
                 const cur = q.shift();
-                 for(const neighbor of adj[cur]){
-                    if(visited[neighbor] !== 1){
-                        visited[neighbor] = 1;
-                        q.push(neighbor);
+                 for(let j = 0; j < n; j++){
+                    if(visited[j] !== 1 && isConnected[j][cur] === 1)
+                    {
+                        visited[j] = 1;
+                        q.push(j);
                     }
                 }
             }
