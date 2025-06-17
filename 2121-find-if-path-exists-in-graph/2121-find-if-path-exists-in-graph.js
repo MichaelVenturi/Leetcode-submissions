bec8f65999d86d38b1331d9ded8a4ea5d0c019dd
Michael Venturi <michaelventuri58@aol.com>
2025-06-17 16:15:01 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    const adj = Array.from({length: n}, () => []);
+    for(const [a,b] of edges){
+        if(a === b) continue;
+        adj[a].push(b);
+        adj[b].push(a);
+    }
+    const visited = new Array(n).fill(0);
+    visited[source] = 1;
+    const q = [...adj[source]];
+    while(q.length > 0) {
+        const cur = q.shift();
+        if(cur === destination) return true;
+        if(visited[cur] === 1) continue;
+        for(const n of adj[cur]){
+            if(visited[n] === 0){
+                visited[n] = 1;
+                q.push(n);
+            }
+        }
+    }
+    return false;
+
+};
